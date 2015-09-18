030a72bde4bb520eefa96c23fda597b8fe76a8bd
LizaKiyan <liza.kiyan1@gmail.com>
2015-09-18 22:10:44 +0300

    Update with icons

--- TaxiClient/app/src/main/java/com/scholota/taxiclient/MainActivity.java
+++ TaxiClient/app/src/main/java/com/scholota/taxiclient/MainActivity.java
@@ -3,6 +3,7 @@ package com.scholota.taxiclient;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Created by Timur on 09.09.2015.
@@ -14,5 +15,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button;
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageDrawable(getResources().getDrawable(R.drawable.ic_magnify_white_48dp));
     }
 }
