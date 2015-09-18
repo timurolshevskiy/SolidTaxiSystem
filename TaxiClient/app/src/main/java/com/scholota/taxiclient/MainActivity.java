2a0c784317a8a2a7fa3d844a89d9e441fc9f9b23
Tymur <timurka101@mail.ru>
2015-09-18 20:37:36 +0300

    add button

--- TaxiClient/app/src/main/java/com/scholota/taxiclient/MainActivity.java
+++ TaxiClient/app/src/main/java/com/scholota/taxiclient/MainActivity.java
@@ -2,6 +2,7 @@ package com.scholota.taxiclient;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 /**
  * Created by Timur on 09.09.2015.
@@ -12,5 +13,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button;
     }
 }
