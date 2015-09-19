78b84062f57dcf8724089e024dd59d1780579532
LizaKiyan <liza.kiyan1@gmail.com>
2015-09-19 15:55:46 +0300

    toolbar

--- TaxiClient/app/src/main/java/com/scholota/taxiclient/MainActivity.java
+++ TaxiClient/app/src/main/java/com/scholota/taxiclient/MainActivity.java
@@ -2,13 +2,17 @@ package com.scholota.taxiclient;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toolbar;
 
 /**
  * Created by Timur on 09.09.2015.
  */
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +20,21 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         Button button;
 
+        initToolbar();
+
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
         imageView.setImageDrawable(getResources().getDrawable(R.drawable.ic_magnify_white_48dp));
+    }
+    private void initToolbar()
+    {
+        toolbar=(Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu_main);
     }
 }
